d452b3ee5c72b5acde03ebdbe031a5c2d24c610d
Ayou0702 <www.2991004793@qq.com>
2023-04-01 17:38:00 +0800

    更新了依赖的版本 不再指定扫描包路径

--- src/main/java/com/enterprise/EnterpriseWxPushApplication.java
+++ src/main/java/com/enterprise/EnterpriseWxPushApplication.java
@@ -1,23 +1,20 @@
 package com.enterprise;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * 项目启动类，配置了扫描路径和自动任务
+ * 项目启动类
  *
  * @author Iwlthxcl
- * @version 1.0
- * @time 2023/3/8 17:00
+ * @version 1.1
  */
-@SpringBootApplication(scanBasePackages = {"com.enterprise"})
-@MapperScan("com.enterprise.mapper")
+@SpringBootApplication()
 @EnableScheduling
 public class EnterpriseWxPushApplication {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(EnterpriseWxPushApplication.class, args);
     }
 
